f20a3754a6b9d1fa6f65dd533b97ca6c2d6f743b
Rajat sharma <rajatsharma369007@gmail.com>
2023-05-20 07:02:33 +0530

    Correction: path reference to Generaldistribution module
    When importing a module in a package, the dot (.) is used to specify the relative path or location of the module within the package. It helps to navigate through the package hierarchy and access the desired module.

--- distributions/Gaussiandistribution.py
+++ distributions/Gaussiandistribution.py
@@ -1,7 +1,7 @@
 import math
 import matplotlib.pyplot as plt
 
-from Generaldistribution import Distribution
+from .Generaldistribution import Distribution
 
 class Gaussian(Distribution):
     """ Gaussian distribution class for calculating and
